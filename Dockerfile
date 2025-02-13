98ff42fcbff1d72b85e9d854c3a5e0fd5fc30444
AyushDhamankar <blockchaindev.ayush@gmail.com>
2025-02-13 21:42:59 +0530

    remove env from TodoApplication.java

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,10 @@ RUN mvn clean package -DskipTests
 FROM openjdk:17.0.1-jdk-slim
 COPY --from=build /target/todo-0.0.1-SNAPSHOT.jar demo.jar
 EXPOSE 8080
+
+# Set environment variables (for local testing only, remove in production)
+ENV DATABASE_URI=${DATABASE_URI}
+ENV DATA_USERNAME=${DATABASE_USER}
+ENV DATA_PASSWORD=${DATABASE_PASS}
+
 ENTRYPOINT ["java","-jar","demo.jar"]
--- src/main/java/com/app/todo/TodoApplication.java
+++ src/main/java/com/app/todo/TodoApplication.java
@@ -1,6 +1,6 @@
 package com.app.todo;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // Load .env file
-        System.setProperty("DATABASE_URI", dotenv.get("DATABASE_URI"));
-        System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
-        System.setProperty("DATABASE_PASS", dotenv.get("DATABASE_PASS"));
+		// Dotenv dotenv = Dotenv.load(); // Load .env file
+        // System.setProperty("DATABASE_URI", dotenv.get("DATABASE_URI"));
+        // System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+        // System.setProperty("DATABASE_PASS", dotenv.get("DATABASE_PASS"));
 
 		SpringApplication.run(TodoApplication.class, args);
 	}
