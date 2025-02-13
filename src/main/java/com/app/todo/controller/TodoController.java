c6e0a9c5c63bf7dbbddb5e7fab4aa332441e2cc7
AyushDhamankar <blockchaindev.ayush@gmail.com>
2025-02-14 00:00:44 +0530

    Cors added netlify link to TodoController.java

--- src/main/java/com/app/todo/controller/TodoController.java
+++ src/main/java/com/app/todo/controller/TodoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@CrossOrigin("http://localhost:5173/")
+@CrossOrigin("https://todo-springboot.netlify.app/")
 public class TodoController {
     @Autowired
     private TodoServices todoServices;
