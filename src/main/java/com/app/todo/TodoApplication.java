970d313fe7011173d2fc8d562582c564a0ef921d
AyushDhamankar <blockchaindev.ayush@gmail.com>
2025-02-13 21:52:22 +0530

    remove comment env from TodoApplication.java

--- src/main/java/com/app/todo/TodoApplication.java
+++ src/main/java/com/app/todo/TodoApplication.java
@@ -1,6 +1,5 @@
 package com.app.todo;
 
-// import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		// Dotenv dotenv = Dotenv.load(); // Load .env file
-        // System.setProperty("DATABASE_URI", dotenv.get("DATABASE_URI"));
-        // System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
-        // System.setProperty("DATABASE_PASS", dotenv.get("DATABASE_PASS"));
-
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
